0786e2c892872e84bf9da0f16c73ba7598ca4a31
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-06-17 12:19:24 -0600

    Initial Commit

---
+++ OhMyDog/OhMyDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhMyDogApp.swift
+//  OhMyDog
+//
+//  Created by Sebastian Rosas Maciel on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct OhMyDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
